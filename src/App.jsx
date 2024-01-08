0d10bc28a3e062870c3faf8bddf9f5a1744f03cb
Roman <chimrom@mail.ru>
2024-01-08 20:25:49 +0300

    add react1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import {Layout} from "./pages/Layout/Layout.jsx";
-import {MainPage} from "./pages/MainPage/MainPage.jsx";
-import {DynamicComponentJS} from "./Tasks/JS/index.jsx";
-import {JSMain} from "./pages/JSMain/JSMain.jsx";
-import {FrontendTask} from "./Tasks/Frontend/FrontendTask.jsx";
-import './App.css'
+import { Layout } from "./pages/Layout/Layout.jsx";
+import { MainPage } from "./pages/MainPage/MainPage.jsx";
+import { DynamicComponentJS } from "./Tasks/JS/index.jsx";
+import { DynamicComponentReact } from "./Tasks/React/index.jsx";
+import { JSMain } from "./pages/JSMain/JSMain.jsx";
+import { ReactMain } from "./pages/ReactMain/ReactMain.jsx";
+import { FrontendTask } from "./Tasks/Frontend/FrontendTask.jsx";
+import "./App.css";
 
 function App() {
-
-  return (
-    <Routes>
-        <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
-            <Route path="/js" element={<JSMain/>} />
-            <Route path="/js/:id" element={<DynamicComponentJS/>} />
-            <Route path="/frontend" element={<FrontendTask/>} />
-        </Route>
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<MainPage />} />
+                <Route path="/js" element={<JSMain />} />
+                <Route path="/js/:id" element={<DynamicComponentJS />} />
+                <Route path="/react" element={<ReactMain />} />
+                <Route path="/react/:id" element={<DynamicComponentReact />} />
+                <Route path="/frontend" element={<FrontendTask />} />
+            </Route>
+        </Routes>
+    );
 }
 
-export default App
+export default App;
---
+++ src/Tasks/React/React1/React1.jsx
@@ -0,0 +1,139 @@
+import { useState } from "react";
+import data from "../../../developers.json";
+import { UserCard } from "../components/UsersCard/UsersCard";
+
+import styles from "./React1.module.css";
+
+export const React1 = () => {
+    const [developers, setDepelopers] = useState(data.developers);
+    const [newDeveloper, setNewDeveloper] = useState({
+        fio: "",
+        level: "",
+        role: "",
+        id: null,
+    });
+    const [filters, setFilters] = useState({
+        fio: "",
+        level: "",
+        role: "",
+    });
+
+    const isFilter = filters.fio && filters.level && filters.role;
+    const filteredDevelopers = isFilter ? filteredData() : developers;
+
+    const handleChange = (event) => {
+        setNewDeveloper((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value,
+        }));
+    };
+
+    const handleChangeFilter = (event) => {
+        setFilters((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value,
+        }));
+    };
+
+    const handleAddNewDeveloper = () => {
+        setDepelopers((prev) => [...prev, { ...newDeveloper, id: Date.now() }]);
+        setNewDeveloper({ ...newDeveloper, fio: "", level: "", role: "" });
+    };
+
+    const handleDelete = (id) => {
+        setDepelopers((prev) => prev.filter((el) => id !== el.id));
+    };
+
+    function filteredData() {
+        return developers.filter((item) => {
+            return (
+                item.fio.toLowerCase().includes(filters.fio.toLowerCase()) &&
+                item.level === filters.level &&
+                item.role === filters.role
+            );
+        });
+    }
+
+    const clearFilter = () => {
+        setFilters({ ...filters, fio: "", level: "", role: "" });
+    };
+
+    return (
+        <div className={styles.wrapper}>
+            <div>
+                создание нового
+                <input
+                    type="text"
+                    name="fio"
+                    onChange={handleChange}
+                    value={newDeveloper.fio}
+                    placeholder="ФИО"
+                />
+                <select
+                    value={newDeveloper.level}
+                    name="level"
+                    onChange={handleChange}
+                >
+                    <option value="">Выберите уровень</option>
+                    <option value="junior">junior</option>
+                    <option value="middle">middle</option>
+                    <option value="senior">senior</option>
+                </select>
+                <select
+                    value={newDeveloper.role}
+                    name="role"
+                    onChange={handleChange}
+                >
+                    <option value="">Выберите роль</option>
+                    <option value="front">front</option>
+                    <option value="back">back</option>
+                </select>
+                <button
+                    onClick={handleAddNewDeveloper}
+                    disabled={
+                        !newDeveloper.fio ||
+                        !newDeveloper.level ||
+                        !newDeveloper.role
+                    }
+                >
+                    add
+                </button>
+            </div>
+            <div>
+                фильтр
+                <input
+                    type="text"
+                    name="fio"
+                    value={filters.fio}
+                    onChange={handleChangeFilter}
+                    placeholder="ФИО"
+                />
+                <select
+                    value={filters.level}
+                    name="level"
+                    onChange={handleChangeFilter}
+                >
+                    <option value="">Выберите уровень</option>
+                    <option value="junior">junior</option>
+                    <option value="middle">middle</option>
+                    <option value="senior">senior</option>
+                </select>
+                <select
+                    value={filters.role}
+                    name="role"
+                    onChange={handleChangeFilter}
+                >
+                    <option value="">Выберите роль</option>
+                    <option value="front">front</option>
+                    <option value="back">back</option>
+                </select>
+                <button onClick={clearFilter}>clear filter</button>
+            </div>
+            <div className={styles.cardList}>
+                {filteredDevelopers.map((el) => (
+                    <UserCard key={el.id} {...el} handleDelete={handleDelete} />
+                ))}
+            </div>
+        </div>
+    );
+};
